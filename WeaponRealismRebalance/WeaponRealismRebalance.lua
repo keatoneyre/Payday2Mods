e8cb0dd54e603b11992c2e0a251ccebfef00f91c
keatoneyre <kdeyre1105@gmail.com>
2020-11-11 23:33:10 -0700

    Payday 2 is crashing, removed all but CAR-4 fire rate change

--- WeaponRealismRebalance/WeaponRealismRebalance.lua
+++ WeaponRealismRebalance/WeaponRealismRebalance.lua
@@ -3,10 +3,6 @@ local old_init = WeaponTweakData.init
 function WeaponTweakData:init(tweak_data)
 old_init(self, tweak_data)
 
-self.new_m4.CLIP_AMMO_MAX = 30
-self.new_m4.NR_CLIPS_MAX = 8
-self.new_m4.AMMO_MAX = self.new_m4.CLIP_AMMO_MAX * self.new_m4.NR_CLIPS_MAX
-self.new_m4.AMMO_PICKUP = self:_pickup_chance(self.new_m4.AMMO_MAX, PICKUP.AR_HIGH_CAPACITY)
 self.new_m4.fire_mode_data = { fire_rate = 0.075 }
 self.new_m4.auto = { fire_rate = 0.075 }
 
